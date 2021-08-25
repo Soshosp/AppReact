23ce936dd3d01216fe741a60c522030f04f5d79f
Soshosp <elthongame@gmail.com>
2021-08-25 14:05:42 +0000

    CommitIntegrantes

--- ReactApp/src/views/Colecao/Colecao.js
+++ ReactApp/src/views/Colecao/Colecao.js
@@ -11,8 +11,12 @@ function Colecao({ navigation }) {
   return (
     <View style={estiloColecao.container}>
       <View style={estiloColecao.borda}>
+        <Text style={estiloColecao.texto}>Integrantes: Elthon Migotto, Guilherme Nunes, Guilherme Brito</Text>
         <Text style={estiloColecao.texto}>S.O.S HOSP</Text>
-        <Text style={estiloColecao.texto}>O App tem como iniciativa facilitar em sua procura por um hospital, proporcionando ao paciente informações que o levam a concluir qual será o hospital com um atendimento mais rápido, para que o mesmo não passe muito tempo em filas ou tenha o constrangimento de não ser atendido.</Text>
+        <Text style={estiloColecao.texto}>O App tem como iniciativa facilitar em sua 
+        procura por um hospital, proporcionando ao paciente informações que o levam a 
+        concluir qual será o hospital com um atendimento mais rápido, para que o mesmo 
+        não passe muito tempo em filas ou tenha o constrangimento de não ser atendido.</Text>
       </View>
         <TouchableOpacity style={estiloColecao.botaoContainer} onPress={voltar}>
           <Text style={estiloColecao.botaoTexto}>Voltar</Text>
